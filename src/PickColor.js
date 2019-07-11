34b57aff823f622cbaee59b42e1f4fa893ce383e
Jinfeng Ye <jye@book.com>
2019-07-11 13:40:12 -0400

    added styling to make both components have an initial look

--- src/PickColor.js
+++ src/PickColor.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function PickColor() {
+    let style={
+        display: "inline-block",
+        border: "1px solid black",
+        width: 400,
+        height: 500,
+        margin: "auto 20px"
+    }
   return (
-    <div className="SelectedColor">
+    <div style={style} className="SelectedColor">
       <h1>Pick a Color</h1>
     </div>
   );
--- src/SelectedColor.js
+++ src/SelectedColor.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function SelectedColor() {
+    let style={
+        display: "inline-block",
+        border: "1px solid black",
+        width: 400,
+        height: 500,
+        margin: "auto"
+    }
   return (
-    <div className="SelectedColor">
+    <div style={style} className="SelectedColor">
       <h1>Selected Color</h1>
     </div>
   );
