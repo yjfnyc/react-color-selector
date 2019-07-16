92e138e57e72e382cf400e2392ca75144e1e32cf
Jinfeng Ye <yjfnyc@gmail.com>
2019-07-16 01:52:57 -0400

    convert hex color code to rgb code

--- src/components/SelectedColor/ColorLabel.js
+++ src/components/SelectedColor/ColorLabel.js
@@ -5,9 +5,14 @@ function ColorLabel({colorCode}){
         marginTop: "18px",
         fontWeight: "bolder"
     };
+    let r = parseInt(colorCode.slice(0,2), 16);
+    let g = parseInt(colorCode.slice(2,4), 16);
+    let b = parseInt(colorCode.slice(4), 16);
+    
     return (
     <div style={style} className="ColorLabel">
-        #{colorCode.toLowerCase()}
+        <p>#{colorCode.toLowerCase()}</p>
+        <p>rgb({r}, {g}, {b})</p>
     </div>
     );
 }
