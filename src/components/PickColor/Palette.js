287408fcf62d89ba7a47c2c2ace3252ff4788591
Jinfeng Ye <yjfnyc@gmail.com>
2019-07-16 01:11:17 -0400

    validate illegitimate hex digit

--- src/components/PickColor/Palette.js
+++ src/components/PickColor/Palette.js
@@ -41,7 +41,15 @@ class Palette extends Component {
     if(colorCode.length === 0){
       return;
     }
-    
+
+    let hexDigits =['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F'];
+    if(colorCode.split('').some(c => !hexDigits.includes(c))){
+      this.setState({
+        wrongInputVisibility: 'visible'
+      });
+      return;
+    }
+
     if(colorCode.length === 3){
       colorCode = colorCode.split('').map(c => c+c ).join('');
       this.colorCode = colorCode;
