7687951467013c782dfaf438ea8139be7f956ffe
Jinfeng Ye <yjfnyc@gmail.com>
2019-07-16 01:58:47 -0400

    added border to the color board

--- src/components/SelectedColor/ColorBoard.js
+++ src/components/SelectedColor/ColorBoard.js
@@ -4,6 +4,7 @@ function ColorBoard({colorCode}) {
     let style = {
         width: 300,
         height: 300,
+        border: "1px solid #e3e3e3",
         backgroundColor: `#${colorCode}`,
         margin: "auto"
 
