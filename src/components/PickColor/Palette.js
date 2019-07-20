b549073601cfc95934470fd9ff9f2f6a3f9b32d2
Jinfeng Ye <yjfnyc@gmail.com>
2019-07-20 02:39:38 -0400

    extract show wrong input code into method showWrongInput

--- src/components/PickColor/Palette.js
+++ src/components/PickColor/Palette.js
@@ -44,9 +44,7 @@ class Palette extends Component {
 
     let hexDigits =['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','A','B','C','D','E','F'];
     if(colorCode.split('').some(c => !hexDigits.includes(c))){
-      this.setState({
-        wrongInputVisibility: 'visible'
-      });
+      this.showWrongInput();
       return;
     }
 
@@ -56,15 +54,11 @@ class Palette extends Component {
     }
 
     if(colorCode.length === 3 || colorCode.length === 6){
-      this.setState({
-        wrongInputVisibility: 'hidden'
-      })
+      this.hideWrongInput();
       this.props.dispatchColorCode(this.colorCode);
       this.highLightPaletteArea(this.colorCode);
     }else{
-      this.setState({
-        wrongInputVisibility: 'visible'
-      })
+      this.showWrongInput();
     }
   }
 
@@ -113,6 +107,12 @@ class Palette extends Component {
   hideWrongInput(){
     this.setState({
       wrongInputVisibility: 'hidden'
+    });
+  }
+
+  showWrongInput(){
+    this.setState({
+      wrongInputVisibility: 'visible'
     });
   }
 
